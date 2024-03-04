42f08b35f45b290f92de32e5e7dfd1ef1cb9aae3
dognellaf <dans-host@ya.ru>
2024-03-05 01:34:49 +0300

    fix

--- frontend/views/account/signup.php
+++ frontend/views/account/signup.php
@@ -31,7 +31,9 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="form_input_wrap">
 
                 <div class="form_input_block">
-                    <?= $form->field($model, 'phone')->textInput(['autofocus' => true, 'placeholder' => $model->getAttributeLabel('username') . ' *'])->label(false); ?>
+                    <?= $form->field($model, 'phone')->widget(\yii\widgets\MaskedInput::className(), [
+	                    'mask' => '+7 (999) 999 99 99',
+                    ])->label(false); ?>
                 </div>
                 <div class="form_input_block submit_block">
                     <?= Html::submitButton(Yii::t('app', 'Зарегистрироваться'), ['class' => 'common_btn black', 'name' => 'signup-button']) ?>
