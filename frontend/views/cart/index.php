2bdb5d1ba432843cf8da3827d602c95dc68cd586
dognellaf <dans-host@ya.ru>
2024-03-05 17:54:47 +0300

    fix

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -16,6 +16,7 @@ $cost_module = \common\entities\Modules::findOne(9);
 ?>
 
 <script src="/js/vendor/jquery.min.js" type="text/javascript"></script>
+<script src="/js/vendor/ion.rangeSlider.min.js" type="text/javascript"></script>
 
 
 <div id="cart" class="cart page padded padded_bottom">
