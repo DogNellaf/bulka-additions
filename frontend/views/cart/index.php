a9d4f0727d452e9bfe5d860b52361f27034daf5e
dognellaf <dans-host@ya.ru>
2024-03-05 17:53:33 +0300

    fix

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -15,7 +15,7 @@ $user = Yii::$app->user->identity;
 $cost_module = \common\entities\Modules::findOne(9);
 ?>
 
-<script src="/js/vendor/jquery-3.1.1.min.js" type="text/javascript"></script>
+<script src="/js/vendor/jquery.min.js" type="text/javascript"></script>
 
 
 <div id="cart" class="cart page padded padded_bottom">
