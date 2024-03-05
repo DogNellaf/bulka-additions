31e60a35cbdf9d5c0e4e5d9e475e738f63737304
dognellaf <dans-host@ya.ru>
2024-03-05 13:21:40 +0300

    fix

--- frontend/controllers/LoyaltyController.php
+++ frontend/controllers/LoyaltyController.php
@@ -61,7 +61,11 @@ class LoyaltyController extends FrontendController
         $user = Yii::$app->user->identity;
 
         $request = Yii::$app->request;
-        $code = $request->get('code');
+        $num1 = $request->get('num1');
+        $num2 = $request->get('num2');
+        $num3 = $request->get('num3');
+        $num4 = $request->get('num4');
+        $code = $num1.$num2.$num3.$num4;
 
         $result = $loyalty->verifyConfirmationCode($user->phone, $code);
 
