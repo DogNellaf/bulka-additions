6694ffd94a70899be0f55ad7394033e1b7cf25cf
dognellaf <dans-host@ya.ru>
2024-03-06 02:05:31 +0300

    fix

--- frontend/views/cart/checkout.php
+++ frontend/views/cart/checkout.php
@@ -443,7 +443,7 @@ $isSumEnoughForDelivery = true;
                                                             <div class="hidden">
                                                                 <?= Service::formatPrice($cart->getBonuses()) ?>
                                                             </div>
-                                                            <span class="checkout_bonuses" data-cost="-<?= Service::formatPrice($cart->getBonuses()) ?>"><span class="ico icon-coins"></span> <?= Service::formatPrice($cart->getBonuses()) ?></span>
+                                                            <span class="checkout_bonuses" data-cost="0"><span class="ico icon-coins"></span> <?= Service::formatPrice($cart->getBonuses()) ?></span>
                                                         </td>
                                                     </tr>
                                                     <tr>
