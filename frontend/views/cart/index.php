aa88c2d2cc54f995417537ce0eadceb328841e4e
dognellaf <dans-host@ya.ru>
2024-03-05 17:58:25 +0300

    test

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -20,7 +20,7 @@ $cost_module = \common\entities\Modules::findOne(9);
 
 
 <div id="cart" class="cart page padded padded_bottom">
-<input type="text" class="js-range-slider" name="my_range" value="" />
+<!-- <input type="text" class="js-range-slider" name="my_range" value="" />
 <script>
   $(".js-range-slider").ionRangeSlider({
         type: "double",
@@ -30,13 +30,13 @@ $cost_module = \common\entities\Modules::findOne(9);
         to: 500,
         grid: true
     });
-</script>
+</script> -->
 
 <!-- <script src="/js/vendor/imagesloaded.pkgd.min.js" type="text/javascript"></script>
 <script src="/js/vendor/slick.min.js" type="text/javascript"></script>
 <script src="/js/vendor/ion.rangeSlider.js" type="text/javascript"></script>
-<script src="/js/vendor/jquery.suggestions.min.js" type="text/javascript"></script>
-<script src="/js/card-script.js" type="text/javascript"></script> -->
+<script src="/js/vendor/jquery.suggestions.min.js" type="text/javascript"></script> -->
+<script src="/js/card-script.js" type="text/javascript?"></script>
     <div class="page_header">
         <div class="wrapper">
             <div class="title title_1 font_2">
