fe46c7c2d59718e798b430b969d13a7e248c22b5
dognellaf <dans-host@ya.ru>
2024-03-05 12:47:19 +0300

    fix

--- frontend/views/loyalty/confirm.php
+++ frontend/views/loyalty/confirm.php
@@ -56,38 +56,3 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
 </div>
-<!-- 
-<div class="site-signup page padded padded_bottom">
-    <div class="wrapper">
-        <div class="form">
-
-            <div class="page_header">
-                <div class="wrapper">
-                    <div class="title title_1 font_2">
-                        <?= \frontend\components\Service::strSplit(Html::encode($this->title)); ?>
-                    </div>
-                </div>
-            </div>
-
-            <p>Заполните следующие поля для регистрации:</p>
-            <br>
-
-            <?php $form = ActiveForm::begin(['id' => 'form-loyalty-register']); ?>
-
-            <div class="form_input_wrap">
-
-                <div class="form_input_block">
-                    <?= $form->field($model, 'phone')->widget(\yii\widgets\MaskedInput::className(), [
-	                    'mask' => '+7 (999) 999 99 99',
-                    ])->label(false); ?>
-                </div>
-                <div class="form_input_block submit_block">
-                    <?= Html::submitButton(Yii::t('app', 'Зарегистрироваться'), ['class' => 'common_btn black', 'name' => 'signup-button']) ?>
-                </div>
-            </div>
-
-
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
-</div> -->
