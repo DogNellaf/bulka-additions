ab376daa7e8a4e27c8ff09c29d4832556418cf0e
dognellaf <dans-host@ya.ru>
2024-03-06 00:39:58 +0300

    fix

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -205,9 +205,14 @@ $cost_module = \common\entities\Modules::findOne(9);
                                     <input type="text" id="slider" class="js-range-slider" value="" />
                                     <script>
                                         $(".js-range-slider").ionRangeSlider({
+                                            type: "single",
                                             min: 0,
                                             max: 1000,
-                                            to: 500,
+                                            from: 500,
+                                            grid: true,
+                                            from_min: 250,      // set min position for FROM handle (replace FROM to TO to change handle)
+                                            from_max: 750,      // set max position for FROM handle
+                                            from_shadow: true   // highlight restriction for FROM handle
                                             onStart: function (data) {
                                                 bonuses.value = slider.value.split(';')[1];
                                             },
